348310b7494524c8c25bc0f0698e6d9c26513db8
lorenzo <yongqing.he@geely.com>
2023-03-17 15:49:35 +0800

    add node support

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var { readFileSync } = require("fs");
+var path = require("path");
+const app = express();
+
+app.use("/api", async (req, res) => {
+  console.log(req.query);
+  const { type, bookName, title, author } = req.query;
+  if (type && bookName) {
+    let filePath = path.join(
+      __dirname,
+      type.replace(",", "/") + "/" + bookName + ".json"
+    );
+    const data = readFileSync(filePath);
+    let jsonData = JSON.parse(data);
+    console.log(title);
+    if (title && title !== "") {
+      jsonData = jsonData.filter((item) => {
+        return item.title.includes(title);
+      });
+      console.log(jsonData.length);
+    }
+    return res.json({ status: 200, data: jsonData });
+  }
+  res.json({ status: 200, data: "test" });
+});
+
+app.use("*", async (req, res) => {
+  res.status(200).set({ "Content-Type": "text/html" }).end("路由错误！");
+});
+
+const port = process.env.SERVER_PORT || 9090;
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
